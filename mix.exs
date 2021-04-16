16020b9d6deea3338264491392d0baddd1c53238
Bernhard Schwarz <schwarz@users.noreply.github.com>
2021-04-16 19:02:26 +0200

    Publish 0.1.0 to Hex

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule Timedot.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/justndv/timedot"
+
   def project do
     [
       app: :timedot,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Timedot",
+      source_url: @url
     ]
   end
 
@@ -23,7 +30,20 @@ defmodule Timedot.MixProject do
     [
       {:nimble_parsec, "~> 1.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 0.4", only: [:dev]}
+      {:dialyxir, "~> 0.4", only: [:dev]},
+      {:ex_doc, "~> 0.24", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Library for working with hledger's timedot format."
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @url}
     ]
   end
 end
